e1f485a1c9c851eaf9354014af6e3e54e92dd55f
cccccroge <pooopooo543@gmail.com>
2019-11-20 02:33:36 +0800

    Add more in main

--- main.py
+++ main.py
@@ -1,21 +1,119 @@
 from itertools import chain
-
 import sys
+
+# dataset
 import nltk
+
+# model
+import gensim
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+
+# machine learning
 import sklearn
 import scipy.stats
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
-
 import sklearn_crfsuite
 from sklearn_crfsuite import scorers
 from sklearn_crfsuite import metrics
 
-# matplotlib inline
+# plot
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-nltk.download('conll2002')
-print(nltk.corpus.conll2002.fileids())
-print(nltk.corpus.conll2002.iob_sents('esp.testa'))
+
+
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+        'postag': postag,
+        'postag[:2]': postag[:2],        
+    }
+    if i > 0:
+        word1 = sent[i-1][0]
+        postag1 = sent[i-1][1]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper(),
+            '-1:postag': postag1,
+            '-1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['BOS'] = True
+        
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        postag1 = sent[i+1][1]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper(),
+            '+1:postag': postag1,
+            '+1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['EOS'] = True
+                
+    return features
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
+if __name__ == "__main__":
+    # nltk.download('conll2002')
+    # print(nltk.corpus.conll2002.fileids())
+    # print(nltk.corpus.conll2002.iob_sents('esp.testa'))
+    train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+    test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testa'))
+
+    X_train = [sent2features(s) for s in train_sents]
+    y_train = [sent2labels(s) for s in train_sents]
+    X_test = [sent2features(s) for s in test_sents]
+    y_test = [sent2labels(s) for s in test_sents]
+
+    #word2vec = KeyedVectors.load_word2vec_format("model/ch.300.bin", binary=True)
+
+    crf = sklearn_crfsuite.CRF(
+        algorithm='lbfgs', 
+        c1=0.1, 
+        c2=0.1, 
+        max_iterations=100, 
+        all_possible_transitions=True
+    )
+    crf.fit(X_train, y_train)
+
+    labels = list(crf.classes_)
+    labels.remove('O')
+    print(labels)
+
+    y_pred = crf.predict(X_test)
+    metrics.flat_f1_score(y_test, y_pred, 
+                        average='weighted', labels=labels)
+
+    # group B and I results
+    sorted_labels = sorted(
+        labels, 
+        key=lambda name: (name[1:], name[0])
+    )
+    print(metrics.flat_classification_report(
+        y_test, y_pred, labels=sorted_labels, digits=3
+    ))
